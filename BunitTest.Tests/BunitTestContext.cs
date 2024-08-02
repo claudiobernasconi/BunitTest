a814abc49f117c78a736a9003f6db340fe426228
Claudio Bernasconi <email@claudio-bernasconi.net>
2024-08-02 13:38:20 +0200

    Implement a static HTML rendering component and test

---
+++ BunitTest.Tests/BunitTestContext.cs
@@ -0,0 +1,10 @@
+﻿using Bunit;
+
+public abstract class BunitTestContext : TestContextWrapper
+{
+    [TestInitialize]
+    public void Setup() => TestContext = new Bunit.TestContext();
+
+    [TestCleanup]
+    public void TearDown() => TestContext?.Dispose();
+}
